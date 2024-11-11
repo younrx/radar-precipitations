5011f4ea20b34e2edbad17f6519bbd1dac4fa879
Youen <git.youn@gmail.com>
2024-11-11 18:05:56 +0100

    release note cache cleaning

--- scripts/release_note.js
+++ scripts/release_note.js
@@ -1,36 +1,37 @@
 // Release note management
 
-
 // Actions to perform when the document is loaded:
 document.addEventListener("DOMContentLoaded", () => {
-    const showReleaseNote = localStorage.getItem("showReleaseNote") != null ? localStorage.getItem("showReleaseNote") : 'true';
-    if (showReleaseNote === 'true') {
-        let releaseNote = document.getElementById('release-note');
-        releaseNote.classList.remove('hidden');
-        releaseNote.innerHTML = `
-            <div>
-                <div class="header">
-                    <p class="title">Nouveauté !</p>
-                </div>
-                <div class="content">
-                    <p>Il est maintenant possible d'ajouter un marqueur sur la carte en effectuant un double-clic.</p>
-                    <a id="close" class="button">Fermer</a>
-                </div>
-            </div>
-        `;
-        // Manage close operation:
-        const closeButton = document.querySelector('#release-note #close');
-        closeButton.addEventListener('click', function(ev) {
-            ev.preventDefault();
-            ev.stopPropagation();
-            releaseNote.remove();
-            localStorage.setItem("showReleaseNote", 'false');
-        });
-        // Block cusrsor creation through clicks on the pop-up:
-        ['click', 'dblclick'].forEach(function(eventName){
-            releaseNote.addEventListener(eventName, function(ev) {
-                ev.stopPropagation();
-            });
-        });
-    }
+    localStorage.removeItem("showReleaseNote");
+
+    // const showReleaseNote = localStorage.getItem("showReleaseNote") != null ? localStorage.getItem("showReleaseNote") : 'true';
+    // if (showReleaseNote === 'true') {
+    //     let releaseNote = document.getElementById('release-note');
+    //     releaseNote.classList.remove('hidden');
+    //     releaseNote.innerHTML = `
+    //         <div>
+    //             <div class="header">
+    //                 <p class="title">Nouveauté !</p>
+    //             </div>
+    //             <div class="content">
+    //                 <p>Il est maintenant possible d'ajouter un marqueur sur la carte en effectuant un double-clic.</p>
+    //                 <a id="close" class="button">Fermer</a>
+    //             </div>
+    //         </div>
+    //     `;
+    //     // Manage close operation:
+    //     const closeButton = document.querySelector('#release-note #close');
+    //     closeButton.addEventListener('click', function(ev) {
+    //         ev.preventDefault();
+    //         ev.stopPropagation();
+    //         releaseNote.remove();
+    //         localStorage.setItem("showReleaseNote", 'false');
+    //     });
+    //     // Block cusrsor creation through clicks on the pop-up:
+    //     ['click', 'dblclick', 'touchstart'].forEach(function(eventName){
+    //         releaseNote.addEventListener(eventName, function(ev) {
+    //             ev.stopPropagation();
+    //         });
+    //     });
+    // }
 });
